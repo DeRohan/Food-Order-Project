b9170d5a74593986c560acacb242ce831515c23c
DeRohan <rohankumar24@icloud.com>
2023-12-06 20:56:55 +0500

    Delete Feedback Updated

--- admin/delete-feedback.php
+++ admin/delete-feedback.php
@@ -5,10 +5,10 @@
         $delete_fd = "DELETE FROM tbl_feedback WHERE id = $id";
         $delete_fd_result = mysqli_query($conn, $delete_fd);
         if($delete_fd_result) {
-            $_SESSION['delete'] = "<div class='success'>Feedback Deleted Successfully (Nerd)!</div>";
+            $_SESSION['delete'] = "<div class='success'>Feedback Deleted Successfully (Nerd) <__></div>";
             header('location:'.$home_url.'admin/manage-feedback.php');
         } else {
-            $_SESSION['delete'] = "<div class='error'>Failed to Delete Feedback (Get Good):(</div>";
+            $_SESSION['delete'] = "<div class='error'>Failed to Delete Feedback (Get Good) :(</div>";
             header('location:'.$home_url.'admin/manage-feedback.php');
         }
     }
