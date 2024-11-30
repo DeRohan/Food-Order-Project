1a74f89dcec409b9d39125c0ba12503267a925a8
DeRohan <rohankumar24@icloud.com>
2024-11-30 16:50:16 +0500

    Conifguration

--- checkout.php
+++ checkout.php
@@ -21,7 +21,7 @@
         $res_id = $row['res_id'];
 
         $order_query = "INSERT INTO tbl_orders SET
-            order_id = $user_id+1,
+            order_id = 14,
             user_id = $user_id,
             restaurant_id = $res_id,
             date = '$ord_date',
@@ -46,7 +46,7 @@
                     $total = $f_price * $quantity;
                     $total_amount = $total_amount + $total;
                     $sql_details = "INSERT INTO tbl_order_details SET
-                        id = $current_order+1,
+                        id = 18,
                         order_id = $current_order,
                         item_id = $f_id,
                         quantity = $quantity,
@@ -56,7 +56,7 @@
                 }
             }
             $sql_tr = "INSERT INTO tbl_transactions SET
-                tr_id = $current_order-1,
+                tr_id = 7,
                 order_id = $current_order,
                 amount = $total_amount,
                 method = '$payment',
--- empty-cart.php
+++ empty-cart.php
@@ -3,5 +3,6 @@
 
     $sql = "DELETE FROM tbl_cart";
     $result = mysqli_query($conn, $sql);
-    header("location:".$home_url);
+    // header("location:".$home_url);
+    echo "<script>window.location.href='" . $home_url . "index.php';</script>";
 ?>
